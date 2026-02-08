55669973c1c5522532c5e3a6dae19f99d95de1c1
UltralyticsAssistant <web@ultralytics.com>
2026-02-08 15:14:35 +0000

    Auto-format by https://ultralytics.com/actions

--- train/trainYOLOv11-GhostCBAM.py
+++ train/trainYOLOv11-GhostCBAM.py
@@ -1,6 +1,6 @@
 # Python 3.14.2
 """
-Pip install: 
+Pip install:
 1. pip install opencv-python
 2.1 (If GPU): pip install torch torchvision --index-url https://download.pytorch.org/whl/cu126
 2.2 (If CPU): pip install torch torchvision
@@ -9,24 +9,36 @@ Pip install:
 5. pip install polars
 6. pip install scipy
 7. pip install requests
-8. pip install thop
+8. pip install thop.
 """
 
-from ultralytics import YOLO
 import torch
 
-if __name__ == '__main__':
+from ultralytics import YOLO
+
+if __name__ == "__main__":
     if torch.cuda.is_available():
         print("GPU is available")
     else:
         print("CPU only")
 
-    # Load yaml model configuration file and weights file 
-    model = YOLO("yolov11n-GhostCBAM.yaml").load("yolo11n-GhostCBAM.pt") 
+    # Load yaml model configuration file and weights file
+    model = YOLO("yolov11n-GhostCBAM.yaml").load("yolo11n-GhostCBAM.pt")
 
     # Set dataset yaml path here
-    dataset_path = "VnVeggie/data.yaml" 
+    dataset_path = "VnVeggie/data.yaml"
 
     # Train the model (Config workers according to CPU/GPU capability/can cause crashes if set too high)
     # Link params: https://docs.ultralytics.com/modes/train/
-    model.train(data=dataset_path, epochs=200, lr0=0.01, momentum=0.9, patience=40, optimizer="SGD", imgsz=640, batch=16, workers=4 , name="yolov11n-GhostCBAM_vnveggie")
+    model.train(
+        data=dataset_path,
+        epochs=200,
+        lr0=0.01,
+        momentum=0.9,
+        patience=40,
+        optimizer="SGD",
+        imgsz=640,
+        batch=16,
+        workers=4,
+        name="yolov11n-GhostCBAM_vnveggie",
+    )
--- validate/validate.py
+++ validate/validate.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
-MODEL_PATH = 'yolov10n.pt'
-IMAGE_PATH = 'VnVeggie/test/images/IMG_20220916_062758_jpg.rf.363a5eeed2f8cd276dc2211c97fc1c17.jpg'
-if __name__ == '__main__':
+MODEL_PATH = "yolov10n.pt"
+IMAGE_PATH = "VnVeggie/test/images/IMG_20220916_062758_jpg.rf.363a5eeed2f8cd276dc2211c97fc1c17.jpg"
+if __name__ == "__main__":
     model = YOLO(MODEL_PATH)
 
     # Validate results on runs\detect\val
-    results = model.val(data='VnVeggie/data.yaml', imgsz=640, batch=16, workers=4, visualize=True)
+    results = model.val(data="VnVeggie/data.yaml", imgsz=640, batch=16, workers=4, visualize=True)
 
     print(results)
 
