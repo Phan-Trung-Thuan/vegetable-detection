840b865bd33026a96bb19293b34cd4bd8a3125fc
Phan Trung Thuận <thuantk000@gmail.com>
2026-02-19 14:45:55 +0700

    debug

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -65,6 +65,7 @@ class Conv(nn.Module):
             act (bool | nn.Module): Activation function.
         """
         super().__init__()
+        print(c1, c2, k, s, p, g, d)
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
